bfda4275fe77c8675e5b1f3cea66afeca157a531
Fauzan Lubis <fauzanfauh45@gmail.com>
2022-02-05 15:14:45 +0700

    Wrong NIM Data Source on Login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -45,7 +45,7 @@ const Login: React.FC<LoginProps> = (props) => {
         message: "Password/Username tidak benar, coba validasikan kembali",
       });
     } else {
-      await splitbee.user.set(NIMtoData(authData?.username!));
+      await splitbee.user.set(NIMtoData(result.data.username));
 
       setData!(result.data);
     }
